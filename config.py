be4d05ffc9ec8408098ae9965cf728872ee15c8e
haodongyu <63580500+haodongyu@users.noreply.github.com>
2020-08-14 15:41:49 +0800

    Delete config.py

--- config.py
+++
@@ -1,4 +0,0 @@
-dir_pools = ['../experiment/AIM_EDSR_x3_TEST_model_best_forensemble', '../experiment/AIM_DDet_x3_TEST2_model12_forensemble', '../experiment/AIM_DDet_x3_TEST2_model18_forensemble',
-             '../experiment/AIM_WDDet_x3_TEST_model_best_forensemble']
-weights = {'../experiment/AIM_EDSR_x3_TEST_model_best_forensemble':1.0, '../experiment/AIM_DDet_x3_TEST2_model12_forensemble': 1.0, '../experiment/AIM_DDet_x3_TEST2_model18_forensemble':1.0, 
-             '../experiment/AIM_WDDet_x3_TEST_model_best_forensemble': 1.0}
